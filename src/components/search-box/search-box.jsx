af80ab2efce204c1b39854ff989816da9ca42381
Mortuza Karzon <karzonku@gmail.com>
2023-07-05 11:41:43 +0600

    class component search-box.js doene

--- src/components/search-box/search-box.jsx
+++ src/components/search-box/search-box.jsx
@@ -1,15 +1,12 @@
-import { Component } from "react";
 import "./search-box.styles.css"
 
 
-class SearchBox extends Component {
-
-  render() {
-    const { onSearchChange } = this.props.onSearchChange;
-    return (<div>
-      <input className="search-box" type="search" onChange={onSearchChange} />
-    </div>)
-  }
+const SearchBox = ({ onSearchChange }) => {
+  return (<div>
+    <input className="search-box" type="search" onChange={onSearchChange} />
+  </div>)
 }
+
+
 
 export default SearchBox;
